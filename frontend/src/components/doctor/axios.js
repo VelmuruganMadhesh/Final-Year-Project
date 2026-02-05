e6387b64205364531f74d39085ede8b353b0cb5f
VelmuruganMadhesh <velukavitha005@gmail.com>
2026-02-05 22:50:44 +0530

    URL To Deploy

--- frontend/src/components/doctor/axios.js
+++ frontend/src/components/doctor/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: "https://final-year-project-backend-yt0p.onrender.com"
 });
 
 export default api;
